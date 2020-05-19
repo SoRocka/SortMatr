f53074bd847f4092bb8340923235606e2f565d05
SoRocka <62294886+SoRocka@users.noreply.github.com>
2020-05-19 23:16:15 +0300

    Add files via upload

---
+++ SortMatrix/InputForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SortMatrix
+{
+    partial class InputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudRows = new System.Windows.Forms.NumericUpDown();
+            this.nudColumns = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btOK = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudMax = new System.Windows.Forms.NumericUpDown();
+            this.nudMin = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudRows
+            // 
+            this.nudRows.Location = new System.Drawing.Point(16, 38);
+            this.nudRows.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudRows.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudRows.Name = "nudRows";
+            this.nudRows.Size = new System.Drawing.Size(221, 22);
+            this.nudRows.TabIndex = 0;
+            this.nudRows.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // nudColumns
+            // 
+            this.nudColumns.Location = new System.Drawing.Point(16, 82);
+            this.nudColumns.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudColumns.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudColumns.Name = "nudColumns";
+            this.nudColumns.Size = new System.Drawing.Size(221, 22);
+            this.nudColumns.TabIndex = 1;
+            this.nudColumns.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Строки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Столбцы";
+            // 
+            // btOK
+            // 
+            this.btOK.Location = new System.Drawing.Point(16, 239);
+            this.btOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(100, 28);
+            this.btOK.TabIndex = 4;
+            this.btOK.Text = "ОК";
+            this.btOK.UseVisualStyleBackColor = true;
+            this.btOK.Click += new System.EventHandler(this.BtOK_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(137, 239);
+            this.btCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(100, 28);
+            this.btCancel.TabIndex = 5;
+            this.btCancel.Text = "Отмена";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.BtCancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Максимум";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Минимум";
+            // 
+            // nudMax
+            // 
+            this.nudMax.Location = new System.Drawing.Point(16, 176);
+            this.nudMax.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudMax.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudMax.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.nudMax.Name = "nudMax";
+            this.nudMax.Size = new System.Drawing.Size(221, 22);
+            this.nudMax.TabIndex = 7;
+            // 
+            // nudMin
+            // 
+            this.nudMin.Location = new System.Drawing.Point(16, 132);
+            this.nudMin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudMin.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudMin.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.nudMin.Name = "nudMin";
+            this.nudMin.Size = new System.Drawing.Size(221, 22);
+            this.nudMin.TabIndex = 6;
+            this.nudMin.ValueChanged += new System.EventHandler(this.nudMin_ValueChanged);
+            // 
+            // InputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 282);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudMax);
+            this.Controls.Add(this.nudMin);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btOK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudColumns);
+            this.Controls.Add(this.nudRows);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "InputForm";
+            this.Text = "InputForm";
+            ((System.ComponentModel.ISupportInitialize)(this.nudRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudRows;
+        private System.Windows.Forms.NumericUpDown nudColumns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btOK;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudMax;
+        private System.Windows.Forms.NumericUpDown nudMin;
+    }
+}
